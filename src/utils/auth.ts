af984c5273ccebf08ffe9c969fe1b1407b1cde4f
Minh Hoang <anhminh10a2hoa@gmail.com>
2021-10-30 02:28:44 +0300

    added decrypt func

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,9 +1,9 @@
 import CryptoJS from 'crypto-js';
 
-export const encryptUserId = (password: string): string => {
+export const decryptUserId = (password: string): string => {
   const secretKey = import.meta.env.VITE_SECRET_KEY?.toString();
   if(secretKey && secretKey !== '') {
-    return CryptoJS.AES.encrypt(password, secretKey).toString();
+    return CryptoJS.AES.decrypt(password, secretKey).toString();
   }
-  return CryptoJS.AES.encrypt(password, "").toString();
+  return '';
 }
